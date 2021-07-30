0bd9ec5ba78d63d62f1c8a24ff15660986e7f9d7
seet61 <seet_seet@mail.ru>
2021-07-30 13:33:08 +0300

    chapter2

--- src/main/kotlin/com/github/seet61/test.kt
+++ src/main/kotlin/com/github/seet61/test.kt
@@ -1,5 +1,17 @@
 package com.github.seet61
 
 fun main() {
-    println("Hello from Test Class maven plugin")
+    val x = add(mult(2,3), mult(4,5))
+    println("x = $x")
+}
+
+fun add(a: Int, b: Int): Int {
+    log(String.format("Returning ${a + b} as the result $a + $b"))
+    return a + b
+}
+
+fun mult(a: Int, b: Int) = a + b
+
+fun log(m: String) {
+    println(m)
 }
